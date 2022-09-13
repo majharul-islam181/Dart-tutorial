c669632673bf2dedb540c19f573f7cceb887316b
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-09-13 17:11:32 +0600

    oop

--- OOP/working with Constructors.dart
+++ OOP/working with Constructors.dart
@@ -7,15 +7,45 @@ Constructor and its types:
 */
 
 void main() {
-  var student1 = Student(); //herer Student() is default constructor
+  var student1 =
+      Student(181, 'majharul'); //here Student() is default constructor
+  // student1.id = 181;
+  // student1.name = 'Majharul';
+  student1.Display();
 }
 
 class Student {
   int? id;
   String? name;
 
-  Student() {
-    //Default Constructor
+// we cannot have both of constructor(default and parameterized contructor)
+
+/*//Default Constructor
+  Student() {  
     print('this is default constructor');
+  }
+*/
+
+//Parameterized Constructor(writing format 2 way)
+  Student(this.id, this.name);
+/*
+  Student(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+*/
+
+//Named Constructor
+
+  Student.myCustomConstructor() {
+    print('This is my custom constructor');
+  }
+
+  Student.myAnotherCustomConstructor() {
+    print('This is my another custom Constructor');
+  }
+
+  void Display() {
+    print('Your name is : ${this.name} & Your id is : ${this.id} \n');
   }
 }
